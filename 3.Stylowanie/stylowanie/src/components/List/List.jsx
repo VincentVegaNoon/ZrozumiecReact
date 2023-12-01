074a4e1e48fc49ee2526a8b99b785321551a2053
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-12-01 10:47:46 +0100

    stylowanie

--- 3.Stylowanie/stylowanie/src/components/List/List.jsx
+++ 3.Stylowanie/stylowanie/src/components/List/List.jsx
@@ -4,29 +4,19 @@ import { StyledButton } from "../styles/StyledButton";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
 
-const StyledList = styled.ul`
-    display: flex;
-    flex-direction: column;
-    list-style: none;
-    gap: 30px;
-    padding: 30px;
-`;
-
 const StyledListItem = styled(Item)`
-    margin-bottom: 50px;
+  margin-bottom: 50px;
 `;
 
 export function List() {
-    return (
-        <>
-            <button onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>
-                Pokaż liczbę emoji
-            </button>
-            <ul>
-                {emojis.map((emoji) => (
-                    <StyledListItem key={emoji} emoji={emoji} />
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <button onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>Pokaż liczbę emoji</button>
+      <ul className="flex flex-col gap-8 p-8">
+        {emojis.map((emoji) => (
+          <StyledListItem key={emoji} emoji={emoji} />
+        ))}
+      </ul>
+    </>
+  );
 }
